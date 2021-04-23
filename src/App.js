8fb6967276f2b9cdc0872d3f988bc924ca4b41b1
Parahat <nepesovparahatt@gmail.com>
2021-04-23 10:55:19 +0300

    Added unmount/mount animation title

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import Navbar from "./components/Navbar";
 import Peoples from "./components/Peoples";
 import Planets from "./components/Planets";
 import "./App.css";
+import { useTransition, animated, config } from "@react-spring/web";
 
 const App = () => {
   const [page, setPage] = useState("planets");
+  const [show, set] = useState(false);
+  const transitions = useTransition(show, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    reverse: show,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!show),
+  });
 
   return (
     <div className="App">
-      <h1>Star Wars</h1>
+      {transitions(
+        (styles, item) =>
+          item && (
+            <animated.div style={styles}>
+              <h1>Star Wars</h1>
+            </animated.div>
+          )
+      )}
       <Navbar setPage={setPage} />
       <div className="content-div">
         {page === "planets" ? <Planets /> : <Peoples />}
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePaginatedQuery } from "react-query";
 import axios from "axios";
 import Planet from "./Planet";
